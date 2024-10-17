425f70c74d4af2ee659a7e9eea653e836617a45f
UnProMan <zero3771@gmail.com>
2024-10-17 14:34:01 +0900

    프로젝트 기본 설정 파일 추가

--- src/main/java/unility/backend/system/config/SecurityConfig.java
+++ src/main/java/unility/backend/system/config/SecurityConfig.java
@@ -1,9 +1,14 @@
 package unility.backend.system.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -11,6 +16,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
+    // TODO 채워야함
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return null;
+    }
 
 }
